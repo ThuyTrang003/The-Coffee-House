0e6b8c92e8ad45036ed852f0f4b9d99002e92d47
thuytrang003 <truongnguyenthuytrang2003@gmail.com>
2024-03-27 10:19:21 +0700

    add login

--- index.js
+++ index.js
@@ -6,4 +6,5 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import Manage from './src/views/Manage';
-AppRegistry.registerComponent(appName, () => Manage);
+import Login from './src/views/Login';
+AppRegistry.registerComponent(appName, () => Login);
