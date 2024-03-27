c8132fd6e29d145a983b60b5d62eae32345137ce
thuytrang003 <truongnguyenthuytrang2003@gmail.com>
2024-03-27 13:03:27 +0700

    update SignUp

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
 import Manage from './src/views/Manage';
 import Login from './src/views/Login';
-AppRegistry.registerComponent(appName, () => Login);
+import SignUp from './src/views/SignUp';
+import OTPVerify from './src/views/OTPVerify';
+AppRegistry.registerComponent(appName, () => OTPVerify);
