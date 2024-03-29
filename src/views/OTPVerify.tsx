e053b5b7204eb8826787908a00ee3c2ed35d2b7a
thuytrang003 <truongnguyenthuytrang2003@gmail.com>
2024-03-29 11:43:08 +0700

    OTPVerify UI

--- src/views/OTPVerify.tsx
+++ src/views/OTPVerify.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, Image, ImageBackground, TextInput, TouchableOpacity, Dimensions } from 'react-native';
+import { OtpInput } from 'react-native-otp-entry'
+const screenWidth = Dimensions.get('window').width;
 
 function OTPVerify(): React.JSX.Element {
-    const screenWidth = Dimensions.get('window').width;
-
+    const focusNext = () => { }
     return (
         <ImageBackground style={styles.background}
             source={require('../images/loginBackground.jpg')} >
@@ -13,16 +14,35 @@ function OTPVerify(): React.JSX.Element {
                         style={{ height: 20, width: 20 }} />
                 </TouchableOpacity>
                 <View style={styles.items}>
-                    <Text style={[styles.fontWeight, { fontSize: 15, color: 'black', marginTop: 20 }]}>
+                    <Text style={[styles.fontWeight, { fontSize: 17, color: 'black', marginTop: 20 }]}>
                         Xác nhận mã OTP</Text>
-                    <Text style={[styles.fontWeight, { fontSize: 13, color: 'black', marginTop: 5 }]}>
+                    <Text style={[styles.fontWeightLight, { fontSize: 13, color: 'black', marginTop: 8 }]}>
                         Mã xác thực đã được gửi đến địa chỉ email</Text>
-
+                    <Text style={[styles.fontWeightLight, { fontSize: 13, color: 'black' }]}>
+                        abc@gmail.com</Text>
+                    <Text style={[styles.fontWeightLight, { fontSize: 13, color: 'black', marginTop: 40, marginBottom:20 }]}>
+                        Nhập mã để tiếp tục</Text>
+                    <OtpInput
+                        numberOfDigits={6}
+                        onTextChange= {(text)=>console.log(text)}
+                        focusColor='#5F374B'
+                        theme={{
+                            pinCodeContainerStyle: {
+                                height: 55,
+                            }                         
+                        }}
+                    />                   
+                    <View style={styles.component1}>
+                        <Text style={[styles.fontWeightLight, { fontSize: 13, color: 'black' }]}>
+                            Bạn không nhận được mã? </Text>
+                        <TouchableOpacity style={{ alignSelf: 'flex-end' }}>
+                            <Text style={[styles.fontWeight, { fontSize: 13, color: 'black' }]}>
+                                Gửi lại</Text>
+                        </TouchableOpacity>
+                    </View>
                 </View>
             </View>
-        </ImageBackground>
-
-
+        </ImageBackground >
     );
 }
 
@@ -40,38 +60,21 @@ const styles = StyleSheet.create({
         margin: 30,
         alignItems: 'center',
     },
+    fontWeightLight: {
+        fontFamily: 'Roboto-Light',
+    },
     fontWeight: {
         fontWeight: 'bold',
     },
-    button: {
-        marginTop: 20,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#5F374B',
-        borderRadius: 8,
-        width: '100%',
-        height: 40,
+    otp: {
+        marginTop: 10,
+        flexDirection: 'row',
+        flexWrap: 'wrap',
     },
     component1: {
-        marginTop: 20,
         flexDirection: 'row',
-        justifyContent: 'center',
-    },
-    textBox: {
-        marginTop: 15,
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        borderColor: 'gainsboro',
-        borderRadius: 8,
-        borderWidth: 1,
-        height: 40,
-        width: '100%',
-    },
-    textInput: {
-        fontSize: 13,
-        marginLeft: 10,
-    },
+        marginTop: 25,
+    }
 });
 
 export default OTPVerify;
