19109981c15069fe59cc42b5ba2074937a6436ec
thuytrang003 <truongnguyenthuytrang2003@gmail.com>
2024-05-28 08:21:26 +0700

    xong trang yêu thích

--- src/views/favourite.tsx
+++ src/views/favourite.tsx
@@ -4,6 +4,7 @@ import Modal from 'react-native-modal';
 import { ScrollView } from 'react-native-gesture-handler';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { Dropdown } from 'react-native-element-dropdown';
+import { FlatList } from "react-native-gesture-handler";
 
 const screenWidth = Dimensions.get('window').width;
 
@@ -21,8 +22,44 @@ function heartButton(tick: boolean): React.JSX.Element {
 }
 
 function GioHang(): React.JSX.Element {
-    const [tick, setTick] = useState(true);
+    //dữ liệu
+    const [data, setData] = useState([
+        { id: '1', name: 'Olong Tứ Quý Vải1', price: '30000', status: true }, //status là trạng thái 
+        { id: '2', name: 'Olong Tứ Quý Vải2', price: '35000', status: true },
+        { id: '3', name: 'Olong Tứ Quý Vải3', price: '40000', status: true },
+        { id: '4', name: 'Olong Tứ Quý Vải4', price: '40000', status: true },
+        { id: '5', name: 'Olong Tứ Quý Vải5', price: '40000', status: true },
+        { id: '6', name: 'Olong Tứ Quý Vải6', price: '40000', status: true },
+        { id: '7', name: 'Olong Tứ Quý Vải7', price: '40000', status: true },
+    ]);
+    const setStatus = (id: string, newStatus: boolean) => {
+        setData(prevData =>
+            prevData.map(item =>
+                item.id === id ? { ...item, status: newStatus } : item
+            )
+        );
+    }
+    const renderItem = ({ item }) => (
+        <View style={styles.titleComponent}>
+            <View style={[styles.item]}>
+                <Image source={require('../images/coffee.png')} //ảnh sp
+                    style={{ height: 60, width: 60, marginRight: 10 }} />
+                <View>
+                    <View style={[styles.categoryInfor]}>
+                        <Text style={[styles.fontWeight, { fontSize: 16, color: 'black' }]}>
+                            {item.name}</Text>
+                    </View>
+                    <Text style={[styles.fontWeightLight, { fontSize: 15, color: 'black', marginBottom: 10 }]}>
+                        {item.price} đ</Text>
 
+                </View>
+                <TouchableOpacity
+                    onPress={() => { setStatus(item.id, !item.status) }} >
+                    {heartButton(item.status)}
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
     //hàm chính
     return (
         <View style={styles.background}>
@@ -32,30 +69,16 @@ function GioHang(): React.JSX.Element {
                 <Text style={[styles.fontWeight, { fontSize: 15, color: 'black', marginLeft: (screenWidth - 230) / 2 }]}>
                     Sản phẩm yêu thích</Text>
             </View>
-            <GestureHandlerRootView style={{ flex: 8 }}>
+            <GestureHandlerRootView>
                 <ScrollView>
-
-                    <View style={styles.titleComponent}>
-                        <View style={[styles.item]}>
-                            <Image source={require('../images/coffee.png')} //chỗ này để ảnh sp
-                                style={{ height: 60, width: 60, marginRight: 10 }} />
-                            <View>
-                                <View style={[styles.categoryInfor]}>
-                                    <Text style={[styles.fontWeight, { fontSize: 16, color: 'black' }]}>
-                                        Olong Tứ Quý Vải Vảiiiiii Vải</Text>
-                                </View>
-                                <Text style={[styles.fontWeightLight, { fontSize: 15, color: 'black', marginBottom: 10 }]}>
-                                    85.000đ</Text>
-
-                            </View>
-                            <TouchableOpacity
-                                onPress={() => { setTick(!tick) }}>
-                                {heartButton(tick)}
-                            </TouchableOpacity>
-                        </View>
-                    </View>
-
                     
+                    {/* chứa component */}
+                    <FlatList
+                        data={data}
+                        renderItem={renderItem}
+                        keyExtractor={item => item.id}
+                    />
+
                 </ScrollView>
             </GestureHandlerRootView>
         </View>
@@ -91,12 +114,6 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         marginBottom: 10,
     },
-    button1: {
-        backgroundColor: '#DBAFA0',
-        borderRadius: 15,
-        paddingVertical: 5,
-        paddingHorizontal: 10,
-    },
     item: {
         flexDirection: 'row',
         justifyContent: 'space-between',
@@ -106,90 +123,19 @@ const styles = StyleSheet.create({
         paddingBottom: 10,
         paddingTop: 10,
     },
-
     categoryInfor: {
         width: (screenWidth) / 7 * 4,
         marginBottom: 10,
         //backgroundColor: 'gray',
     },
-    totalPay: {
-        flex: 1,
-        justifyContent: 'flex-end',
-        borderColor: 'lightgray',
-        borderTopWidth: 2,
-        paddingTop: 15,
-    },
-    button: {
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#5F374B',
-        borderRadius: 8,
-        width: '100%',
-        height: 40,
-    },
-    modal: {
-        justifyContent: 'flex-end',
-        margin: 0,
-    },
-    modalContent: {
-        backgroundColor: 'white',
-        padding: 20,
-        borderTopLeftRadius: 20,
-        borderTopRightRadius: 20,
-        //height: Dimensions.get('window').height * 0.4,
-    },
-    phuongThucDatHang: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        borderBottomColor: 'lightgray',
-        borderBottomWidth: 1,
-        paddingBottom: 10,
-    },
-    sizeButton: {
-        borderColor: 'lightgray',
-        borderWidth: 1,
-        alignItems: 'center',
-        height: 25,
-        width: 50,
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-    },
-    //dropdown
-    dropdown: {
-        height: 30,
-        width: 60,
-        borderColor: 'black',
-        borderWidth: 0.5,
-        borderRadius: 8,
-        paddingHorizontal: 8,
-    },
-    placeholderStyle: {
-        fontSize: 13,
-    },
-    selectedTextStyle: {
-        fontSize: 13,
-        color: 'black',
-    },
-    iconStyle: {
-        width: 20,
-        height: 20,
-    },
-    itemTextStyle: {
-        fontSize: 13,
-        color: 'black',
-    },
-    quantityButton: {
-        height: 30,
-        width: 80,
-        borderColor: 'black',
-        borderWidth: 0.5,
-        borderRadius: 8,
-        paddingHorizontal: 8,
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        marginLeft: 10,
-        alignItems: 'center'
-    },
+
+
+
+
+
+
+
+
 });
 
 export default GioHang;
